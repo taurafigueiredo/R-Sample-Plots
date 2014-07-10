fc153d9d8226ae5e816f6db15ce84c4eb33e650e
taurafigueiredo <taurafigueiredo@gmail.com>
2014-07-10 20:52:45 -0300

    Plot1 full script (download/unzip/load data/plot graph)

--- plot1.R
+++ plot1.R
@@ -1,8 +1,13 @@
 function () 
 {
-    library(data.table)
-    data <- fread("power_consumption.txt", na.strings = "?", 
-        skip = 66637, nrows = 2880)
+    temp <- tempfile()
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+        destfile = temp)
+    data <- read.table(unz(temp, "household_power_consumption.txt"), 
+        na.strings = "?", skip = 66637, nrows = 2880, sep = ";")
     hist(x = data$V3, col = "red", main = "Global Active Power", 
         xlab = "Global Active Power (killowatts)")
+    unlink(temp)
+    rm(temp)
+    rm(data)
 }
